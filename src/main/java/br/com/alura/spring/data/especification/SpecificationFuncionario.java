4c511bc164c9bda50dd154bf53498a024672ca5f
Valdano Simão <valdanosimao@gmail.com>
2022-05-09 21:00:48 -0300

    compondo specifications

--- src/main/java/br/com/alura/spring/data/especification/SpecificationFuncionario.java
+++ src/main/java/br/com/alura/spring/data/especification/SpecificationFuncionario.java
@@ -1,5 +1,7 @@
 package br.com.alura.spring.data.especification;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import br.com.alura.spring.data.orm.Funcionario;
@@ -11,4 +13,18 @@ public class SpecificationFuncionario {
 			criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
 	}
 	
+	public static Specification<Funcionario> cpf(String cpf){
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.equal(root.get("cpf"), cpf);
+	}
+	
+	public static Specification<Funcionario> salario(Double salario){
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.greaterThan(root.get("salario"), salario);
+	}
+	
+	public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao){
+		return (root, criteriaQuery, criteriaBuilder) -> 
+			criteriaBuilder.greaterThan(root.get("dataContratacao"), dataContratacao);
+	}
 }
--- src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -1,27 +1,66 @@
 package br.com.alura.spring.data.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
+import org.hibernate.sql.ordering.antlr.CollationSpecification;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.especification.SpecificationFuncionario;
 import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
+@Service
 public class RelatorioFuncionarioDinamico {
-	
+
 	private final FuncionarioRepository funcionarioRepository;
 
 	public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
 		this.funcionarioRepository = funcionarioRepository;
 	}
-	
+
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 	public void inicial(Scanner scanner) {
 		System.out.println("Digite o nome");
 		String nome = scanner.next();
-		
-		List<Funcionario> funcionarios = funcionarioRepository.findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+
+		if (nome.equalsIgnoreCase("NULL")) {
+			nome = null;
+		}
+
+		System.out.println("Digite o cpf");
+		String cpf = scanner.next();
+
+		if (nome.equalsIgnoreCase("NULL")) {
+			cpf = null;
+		}
+
+		System.out.println("Digite o salario");
+		Double salario = scanner.nextDouble();
+
+		if (salario == 0) {
+			salario = null;
+		}
+
+		System.out.println("Digite a data de contratacao");
+		String data = scanner.next();
+
+		LocalDate dataContratacao;
+		if (data.equalsIgnoreCase("NULL")) {
+			dataContratacao = null;
+		} else {
+			dataContratacao = LocalDate.parse(data, formatter);
+		}
+
+		List<Funcionario> funcionarios = funcionarioRepository
+				.findAll(Specification.where(SpecificationFuncionario.nome(nome)).or(SpecificationFuncionario.cpf(cpf))
+						.or(SpecificationFuncionario.salario(salario))
+						.or(SpecificationFuncionario.dataContratacao(dataContratacao)));
+		funcionarios.forEach(System.out::println);
 	}
-	
+
 }
